0ed5f45eda87c211bf2863f51402d8a314c194a6
q <q@mail.com>
2018-03-19 12:58:33 -0600

    commented

--- endian.c
+++ endian.c
@@ -9,14 +9,18 @@ int main() {
 	np = &n;
 	int i;	//loop index
 
+	//0 in LSbyte, 1 in next least signifigant byte etc
 	for (i=0; i<sizeof(int); i++) {
 		n += (i*pow(2, 8*(i)));
 	}	
-
+	
+	//isolate bytes and print from fist memory address to second etc
+	//litte endian counts upward, big endian downward
 	for (i=0; i<sizeof(int); i++) {
 		printf("%d\n", *((char*)np + i));
 	}
-
+	
+	//first memory address LSB or MSB?
 	if ( *((char*)np + 0) == 0 ) 
 		printf("little endian\n");
 	else 
